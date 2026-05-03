fafdd016cd013155e5bab57a3304d0ab834256cc
AgbajiAlexander <alexanderogah@gmail.com>
2026-05-03 01:56:13 +0100

    working with two or more go files

---
+++ note/join.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+var names = []string{"Ogah", "Alex", "Edwin", "Ruth"}
+
+func familyMembers(n string) {
+	fmt.Println("You are welcome", n)
+}
+
+func myScore() {
+	fmt.Println("i scored this number as a memeber of the family", score)
+}
---
+++ note/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+var score = 590.45
+
+func main() {
+
+	familyMembers("to Agbaji Solomon's Family")
+
+	for _, word := range names {
+		fmt.Println(word)
+	}
+
+	myScore()
+
+}
